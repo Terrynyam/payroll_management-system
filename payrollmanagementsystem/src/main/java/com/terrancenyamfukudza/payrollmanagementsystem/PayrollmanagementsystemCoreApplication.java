8517430df3326d5b83ef70e72604ddf320810f94
Terranceny <terrancen@afrosoft.co.zw>
2023-03-08 17:28:29 +0200

    initial commit

---
+++ payrollmanagementsystem/src/main/java/com/terrancenyamfukudza/payrollmanagementsystem/PayrollmanagementsystemCoreApplication.java
@@ -0,0 +1,13 @@
+package com.terrancenyamfukudza.payrollmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollmanagementsystemCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollmanagementsystemCoreApplication.class, args);
+	}
+
+}
---
+++ payrollmanagementsystem/src/test/java/com/terrancenyamfukudza/payrollmanagementsystem/PayrollmanagementsystemCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terrancenyamfukudza.payrollmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollmanagementsystemCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
